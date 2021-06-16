794c3570694db0188528d0fc50e4399c5f90a391
highdanny11 <highdanny11@gmail.com>
2021-06-16 13:16:58 +0800

    add dir

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,5 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/production-sub-path/'
     : '/',
-    assetsSubDirectory:'dist',
-    assetsPublicPath: '/week6-vue-cli/dist/'
+    assetsDir:'dist'
 }
