ec39d5725a00d6c5cd891b82dde0d5eb9a1538e6
highdanny11 <highdanny11@gmail.com>
2021-06-16 14:46:59 +0800

    edit45

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="#">商城</a>
       <button
         class="navbar-toggler"
         type="button"
