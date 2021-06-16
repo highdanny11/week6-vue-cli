6bdc1a353f54a8ccc1bb7cee2bd482fee610bcda
highdanny11 <highdanny11@gmail.com>
2021-06-16 13:12:37 +0800

    add edit1

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/production-sub-path/'
     : '/',
+    assetsSubDirectory:'dist',
     assetsPublicPath: '/week6-vue-cli/dist/'
 }
